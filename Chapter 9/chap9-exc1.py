a36fd57a72e8d5e4abc65d8c8b985b8037957a41
Marco Obaid <marco.obaid@gmail.com>
2024-08-04 10:39:02 -0500

    Starting Chapter 9

---
+++ Chapter 9/chap9-exc1.py
@@ -0,0 +1,6 @@
+# Write a program that reads the words in words.txt and stores them as keys in a dictionary. 
+# It doesn’t matter what the values are. Then you can use the in operator as a fast way to check 
+# whether a string is in the dictionary.
+# www.py4e.com/code3/words.txt
+#
+
