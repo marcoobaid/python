4a3dcccd1c1b9c1daa6b0a41d563559e38737f80
Marco Obaid <marco.obaid@gmail.com>
2024-09-03 22:51:25 -0500

    Chapter 13, Assignment 1

---
+++ Chapter 13/chap13-assignment-1.py
@@ -0,0 +1,32 @@
+# In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/xml3.py.
+# The program will prompt for a URL, read the XML data from that URL using urllib and then parse and 
+# extract the comment counts from the XML data, compute the sum of the numbers in the file.
+#
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+#
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_2037191.xml (Sum ends with 24)
+#
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_2037191.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+for result in counts:
+    # Debug print the data :)
+    #print(result.text)
+    nums.append(int(result.text))
+
+print('Count:', len(counts))
+print('Sum:', sum(nums))
+#print(nums)
